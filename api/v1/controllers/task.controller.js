5df6080c7e408767dde8fa8a6289490e8277e219
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-11-27 19:28:21 +0700

    Search and find

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,31 +1,56 @@
-const Task= require("../models/task.model");
-
-
+const Task = require("../models/task.model");
+const paginationHelpers= require("../../../helpers/pagination");
+const searchHelpers=require("../../../helpers/search");
 // [GET] /api/v1/tasks
-module.exports.index= async (req, res, next) =>{
-    const find={
-        deleted:false
-    }
-    if(req.query.status){
-       find.status=req.query.status;
-        res.json(task);
-    }
-    const task= await Task.find(find);
+module.exports.index = async (req, res, next) => {
+  const find = {
+    deleted: false,
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
     res.json(task);
-}
+  }
 
+  const objectSearch = searchHelpers(req.query);
+
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+
+
+  const countTotalItems = await Task.countDocuments(find);
+
+  let objectPagination = paginationHelpers(
+    {
+      currentPage: 1,
+      limitItems: 2,
+    },
+    req.query,
+    countTotalItems
+  );
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+
+  const task = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
+  res.json(task);
+};
 
 // [GET] /api/v1/tasks/:id
 
-module.exports.detail= async (req, res, next) =>{
-    try {
-        const id= req.params.id;
-        const task= await Task.findOne({
-            _id:id,
-            deleted:false
-        });
-        res.json(task);
-    } catch (error) {
-        res.json(error);
-    }
+module.exports.detail = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findOne({
+      _id: id,
+      deleted: false,
+    });
+    res.json(task);
+  } catch (error) {
+    res.json(error);
+  }
 };
