74c89426ffd3bf0b2d94bbac45c4e7c5dae41e38
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-11-28 10:07:10 +0700

    Install cors

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const database = require("./config/database");
-const bodyParser = require('body-parser');
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routesApiVer1= require("./api/v1/routes/task.route");
 require("dotenv").config();
 // const Task= require("./models/task.model")
 
-database.connect();
 const app = express();
-
 const port = process.env.PORT;
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
 
+database.connect();
 
 
-
+// routesApiVer1(app);
 
 app.listen(port, () => {
     console.log(`Example app Ongoing on port ${port}`);
